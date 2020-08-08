97c429bf02adcb3a4c6787cd935257ecd28b2812
hama3254 <hama32546@web.de>
2020-08-08 19:13:38 +0200

    v0.2 update
    fix microphone permission
    added detailt settings descriptions

--- popup.js
+++ popup.js
@@ -13,17 +13,17 @@ function InjectJS() {
         if (radio_btn[i].checked) {
             //alert(radio_btn[i].value);
             var selectedDevice = radio_btn[i].value;
+            //alert("navigator.mediaDevices.enumerateDevices().then(function(devices){const audioDevices=devices.filter(device=>device.kind==='audiooutput');const audioDevicesUse=devices.filter(device=>device.label==='" + selectedDevice + "');var video=document.querySelector('video');var p2=new Promise(function(resolve,reject){video.setSinkId(audioDevicesUse[0].deviceId)});p2.then(console.log('the end'))}).catch(function(err){console.log(err.name+': '+err.message)});")
             browser.tabs.executeScript({
-                code: "navigator.mediaDevices.enumerateDevices().then(function(devices){const audioDevices=devices.filter(device=>device.kind==='audiooutput');const audioDevicesUse=devices.filter(device=>device.label==='" + selectedDevice + "');var video=document.querySelector('video');var p2=new Promise(function(resolve,reject){video.setSinkId(audioDevicesUse[0].deviceId)});p2.then(console.log('the end'))}).catch(function(err){console.log(err.name+': '+err.message)});"
-           //multi line code at the end as comment
+                code: "navigator.mediaDevices.enumerateDevices().then(function(devices){const audioDevices=devices.filter(device=>device.kind==='audiooutput');var video=document.querySelector('video');var p2=new Promise(function(resolve,reject){video.setSinkId(audioDevices[" + selectedDevice + "].deviceId)});p2.then(console.log('the end'))}).catch(function(err){console.log(err.name+': '+err.message)});"
+                //multi line code at the end as comment
 
- });
-           
+            });
+
         }
     }
-window.close()
+    window.close()
 }
-
 navigator.mediaDevices.getUserMedia({
     audio: true
 })
@@ -43,12 +43,12 @@ navigator.mediaDevices.getUserMedia({
 
         var i;
         for (i = 0; i < audioDevices.length; i++) {
-
+            var DeviceCheckID = "";
+            DeviceCheckID = audioDevices[i].deviceId;
             var radiobox = document.createElement('input');
             radiobox.type = 'radio';
             radiobox.id = 'Audio' + i;
-            radiobox.value = audioDevices[i].label;
-
+            radiobox.value = i;
             radiobox.className = 'option-input radio';
             radiobox.onclick = InjectJS;
             var label = document.createElement('label')
@@ -67,6 +67,7 @@ navigator.mediaDevices.getUserMedia({
     .catch(function (err) {
         console.log(err.name + ': ' + err.message);
     });
+
 })
 .catch(function (err) {
     console.log('No mic for you!')
@@ -76,30 +77,65 @@ navigator.mediaDevices.getUserMedia({
     h2.innerHTML = 'Please open the settings';
 
     document.getElementById('frame2').appendChild(h2);
-});
 
+    navigator.mediaDevices.enumerateDevices()
+    .then(function (devices) {
+        const audioDevices = devices.filter(device => device.kind === 'audiooutput');
+        console.log(audioDevices);
+        var container = document.getElementById('frame2');
+
+        var i;
+        for (i = 0; i < audioDevices.length; i++) {
+
+            var radiobox = document.createElement('input');
+            radiobox.type = 'radio';
+            radiobox.id = 'Audio' + i;
+            radiobox.value = i;
+
+            radiobox.className = 'option-input radio';
+            radiobox.onclick = InjectJS;
+            var label = document.createElement('label')
+                label.htmlFor = 'Audio' + i;
+            var description = document.createTextNode('I have no name but i work!'); //audioDevices[i].label);
+            label.appendChild(description);
+
+            var vbNewLine = document.createElement('br')
+
+                container.appendChild(radiobox);
+            container.appendChild(label);
+            container.appendChild(vbNewLine);
+        }
+
+    })
+    .catch(function (err) {
+        console.log(err.name + ': ' + err.message);
+    });
+
+});
 
 /*
 
 navigator.mediaDevices.enumerateDevices()
 .then(function(devices) {
- 	const audioDevices = devices.filter(device => device.kind === 'audiooutput');
-	const audioDevicesUse = devices.filter(device => device.label=== '" + selectedDevice + "');
-	var video = document.querySelector('video');
-	var p2 = new Promise(
-    // Resolver-Funktion kann den Promise sowohl auflösen als auch verwerfen
-    // reject the promise
-    function(resolve, reject) {       
-      video.setSinkId(audioDevicesUse[0].deviceId)
-    });
+const audioDevices = devices.filter(device => device.kind === 'audiooutput');
+const audioDevicesUse = devices.filter(device => device.label=== '" + selectedDevice + "');
+var video = document.querySelector('video');
+var p2 = new Promise(
+// Resolver-Funktion kann den Promise sowohl auflösen als auch verwerfen
+// reject the promise
+function(resolve, reject) {
+video.setSinkId(audioDevicesUse[0].deviceId)
+});
 
-  p2.then(
-	console.log('the end')
-  
-   );
+p2.then(
+console.log('the end')
+
+);
 })
 .catch(function(err) {
-  console.log(err.name + ': ' + err.message);
-}); 
+console.log(err.name + ': ' + err.message);
+});
+
+
 
 */
--- settings.js
+++ settings.js
@@ -7,7 +7,29 @@ navigator.mediaDevices.getUserMedia({ audio: true })
       })
       .catch(function(err) {
         console.log('No mic for you!')
-      });
+		
+	var h3 = document.createElement('h3');
+    h3.id = 'bulgy-radios-label';
+    h3.innerHTML = "<br>It looks like you denied the acces to the microphone.<br>That's ok, you can still use the Plugin but i can not display the device names";
+    document.getElementById('frame2').appendChild(h3);
+	
+	var h3_1 = document.createElement('a');
+    h3_1.id = 'linkclass';
+	h3_1.className = 'linkclass';
+    h3_1.innerHTML = '<br>To quote from the developer site:<br>"For security reasons, the label is always an empty string ("")<br> if the user has not obtained permission to use at least one media device,<br> either by starting a stream from the microphone or camera, or by persistent permissions being granted."';
+	h3_1.href = "https://developer.mozilla.org/en-US/docs/Web/API/MediaDeviceInfo/label";
+   document.getElementById('frame2').appendChild(h3_1);
+     
+	var h3_2 = document.createElement('h3');
+    h3_2.id = 'bulgy-radios-label';
+    h3_2.innerHTML = '<br>You can reverse this action on any time.';
+    document.getElementById('frame2').appendChild(h3_2);
+	
+	var img = document.createElement("img");
+	img.src = "/images/FAQ1.png";
+	document.getElementById('frame2').appendChild(img);
+	
+	 });
 
 
 }
